367b8074606ee51b67ea4585ee83ed81b448dc63
Logan Barnes <logan_barnes@outlook.com>
2017-09-12 00:23:52 +0100

    Refactored to use new shared sim

---
+++ src/exec/main.cpp
@@ -0,0 +1,18 @@
+#include <vmp/SimTest.hpp>
+//#include <kfr/math.hpp>
+
+int main(const int argc, const char *argv[])
+{
+    try
+    {
+        sim::SimInitData initData;
+        initData.title = "Mesh Sim";
+        sim::OpenGLSimulation<Simulator>(initData).runNoFasterThanRealTimeLoop();
+    }
+    catch (const std::exception &e)
+    {
+        std::cerr << "Program failed: " << e.what() << std::endl;
+        return EXIT_FAILURE;
+    }
+    return EXIT_SUCCESS;
+}
---
+++ src/vmp/SimTest.cpp
@@ -0,0 +1,53 @@
+#include <vmp/SimTest.hpp>
+#include <sim-driver/meshes/MeshFunctions.hpp>
+
+Simulator::Simulator(int, int, sim::SimData *pSimData)
+    : renderer_{sim::PosNormTexMesh(sim::create_sphere_mesh_data < sim::PosNormTexVertex > )},
+      simData_{*pSimData}
+{
+    simData_.camera.setUsingOrbitMode(true);
+    simData_.camera.setOrbitOrigin({0, 0, 0});
+    simData_.camera.setOrbitOffsetDistance(5);
+
+    prevCam_ = simData_.camera;
+}
+
+void Simulator::onUpdate(double, double timeStep)
+{
+    prevCam_ = simData_.camera;
+    simData_.camera.yaw(static_cast<float>(timeStep));
+}
+
+void Simulator::onRender(int, int, double alpha)
+{
+    auto a = static_cast<float>(alpha);
+
+    sim::Camera camera;
+    glm::vec3 eye{glm::mix(prevCam_.getEyeVector(), simData_.camera.getEyeVector(), a)};
+    glm::vec3 look{glm::mix(prevCam_.getLookVector(), simData_.camera.getLookVector(), a)};
+    glm::vec3 up{glm::mix(prevCam_.getUpVector(), simData_.camera.getUpVector(), a)};
+
+    float aspect{glm::mix(prevCam_.getAspectRatio(), simData_.camera.getAspectRatio(), a)};
+
+    camera.lookAt(eye, eye + look, up);
+    camera.setAspectRatio(aspect);
+
+    renderer_.render(a, camera);
+}
+
+void Simulator::onGuiRender(int, int)
+{
+    ImGui::SetNextWindowPos(ImVec2(0, 0));
+    if (ImGui::Begin("Window", nullptr, ImGuiWindowFlags_AlwaysAutoResize))
+    {
+        ImGui::Text("Framerate: %.3f ms/frame", 1000.0f / ImGui::GetIO().Framerate);
+        renderer_.configureGui();
+    }
+    ImGui::End();
+}
+
+void Simulator::keyCallback(GLFWwindow *, int, int, int, int mods, const sim::SimCallbacks <Simulator> &)
+{
+    simData_.paused = (mods == GLFW_MOD_SHIFT);
+}
+

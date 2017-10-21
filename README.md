93e4a73e3a2dc1e15ce8f06ea2b8c0217192a6c5
Logan Barnes <barneslt63@gmail.com>
2017-10-21 07:25:24 -0700

    Testing linux setup on travis

--- README.md
+++ README.md
@@ -5,3 +5,11 @@ The Visual Music Project
 | ------ | -------- | ------ |
 | OS X   | AppleClang 7.3 | [![Travis CI](https://travis-ci.org/LoganBarnes/AirWaves.svg?branch=master)](https://travis-ci.org/LoganBarnes/AirWaves)|
 | Windows 64 bit | Visual Studio 15 | [![Build status](https://ci.appveyor.com/api/projects/status/2qojm6s3i7ymwaac?svg=true)](https://ci.appveyor.com/project/LoganBarnes/airwaves) |
+
+Linux
+-----
+Required packages if using ALSA (default):
+
+```bash
+sudo apt-get install libasound2 alsa-utils alsa-oss libasound2-dev
+```
---
+++ src/exec/simple_sound_test.cpp
@@ -0,0 +1,142 @@
+#include <cmath>
+#include "RtAudio.h"
+
+void print_usage() {
+    std::cout << "Usage:\n"
+              << "./sound-test [options]\n\n"
+              << "Options:\n"
+              << "\t-h,--help   show this message\n"
+              << "\t-s,--sine   play sine waveform (default)\n"
+              << "\t-w,--saw    play sawtooth waveform"
+              << std::endl;
+}
+
+template<typename T>
+class Sound
+{
+public:
+    explicit Sound(bool sine = true)
+        : sine_(sine)
+    {}
+
+    static int play_sound(void *outputBuffer,
+                          void *,
+                          unsigned int nBufferFrames,
+                          double,
+                          RtAudioStreamStatus status,
+                          void *userData)
+    {
+        auto *buffer = static_cast<T *>(outputBuffer);
+        auto *s = static_cast<Sound<T> *>(userData);
+
+        if (status) {
+            std::cout << "Stream underflow detected!" << std::endl;
+        }
+
+        return s->play_sound(buffer, nBufferFrames, status);
+    }
+
+private:
+    bool sine_;
+    std::vector<T> last_values_{0, 0};
+    T max_amplitude_{0.2};
+    T freq_scale_{0.025};
+
+    int play_sound(T *outputBuffer, unsigned int nBufferFrames, RtAudioStreamStatus status)
+    {
+        return sine_ ? sine(outputBuffer, nBufferFrames, status) : saw(outputBuffer, nBufferFrames, status);
+    }
+
+    /**
+     * @brief Two-channel sawtooth wave generator.
+     */
+    int saw(T *outputBuffer, unsigned int nBufferFrames, RtAudioStreamStatus status)
+    {
+        // Write interleaved audio data.
+        for (unsigned i = 0; i < nBufferFrames; i++) {
+            for (unsigned j = 0; j < 2; j++) {
+                *outputBuffer++ = last_values_[j];
+                last_values_[j] += freq_scale_ * max_amplitude_ / T(2);
+                if (last_values_[j] >= max_amplitude_) last_values_[j] -= T(2) * max_amplitude_;
+            }
+        }
+    }
+
+    /**
+     * @brief Two-channel sine wave generator.
+     */
+    int sine(T *outputBuffer, unsigned int nBufferFrames, RtAudioStreamStatus status)
+    {
+        // Write interleaved audio data.
+        for (unsigned i = 0; i < nBufferFrames; i++) {
+            for (unsigned j = 0; j < 2; j++) {
+                *outputBuffer++ = std::sin(last_values_[j]) * max_amplitude_ * T(3);
+                last_values_[j] += freq_scale_;
+                if (last_values_[j] >= T(M_PI)) last_values_[j] -= T(2) * T(M_PI);
+            }
+        }
+        return 0;
+    }
+};
+
+int main(const int argc, const char *argv[])
+{
+    bool use_sine = true;
+    for (int argi = 1; argi < argc; ++argi) {
+        std::string arg(argv[argi]);
+        if (arg == "-s" || arg == "--sine") {
+            use_sine = true;
+        } else if (arg == "-w" || arg == "--saw") {
+            use_sine = false;
+        } else if (arg == "-h" || arg == "--help") {
+            print_usage();
+            return EXIT_SUCCESS;
+        } else {
+            std::cout << "Unrecognised option: '" << arg << "'\n";
+            print_usage();
+            return EXIT_SUCCESS;
+        }
+    }
+    RtAudio dac;
+    if (dac.getDeviceCount() < 1) {
+        std::cout << "\nNo audio devices found!\n";
+        return EXIT_FAILURE;
+    }
+
+    RtAudio::StreamParameters parameters;
+    parameters.deviceId = dac.getDefaultOutputDevice();
+    parameters.nChannels = 2;
+    parameters.firstChannel = 0;
+    unsigned sampleRate = 44100;
+    unsigned bufferFrames = 256; // 256 sample frames
+    Sound<double> s(use_sine);
+    try {
+        dac.openStream(&parameters,
+                       nullptr,
+                       RTAUDIO_FLOAT64,
+                       sampleRate,
+                       &bufferFrames,
+                       &Sound<double>::play_sound,
+                       &s);
+        dac.startStream();
+    }
+    catch (const RtAudioError &e) {
+        e.printMessage();
+        return EXIT_FAILURE;
+    }
+
+    char input;
+    std::cout << "\nPlaying ... press <enter> to quit.\n";
+    std::cin.get(input);
+    try {
+        // Stop the stream
+        dac.stopStream();
+    }
+    catch (const RtAudioError &e) {
+        e.printMessage();
+        return EXIT_FAILURE;
+    }
+    if (dac.isStreamOpen()) { dac.closeStream(); }
+
+    return EXIT_SUCCESS;
+}
